67b2b7bb0e0b88ecbe0cebda35620c6db71bb45a
Areo Ayomide <areoayomide2008@gmail.com>
2025-06-24 00:18:55 +0100

    style: update text color for improved task visibility

--- app/page.js
+++ app/page.js
@@ -102,7 +102,7 @@ export default function Home() {
                 />
                 <span
                   className={`text-lg ${
-                    checked ? "line-through text-gray-400" : ""
+                    checked ? "line-through text-gray-200" : ""
                   }`}
                 >
                   {task}
